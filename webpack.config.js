938b600297132672aa56515768c4f6144d1fc1a4
MT4848 <152455256+MT4848@users.noreply.github.com>
2026-03-06 14:02:25 -0500

    app setup with react + webpack 5 + typescript

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.tsx",
+
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    clean: true
+  },
+
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"]
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+
+  devServer: {
+    port: 3000,
+    open: true,
+    hot: true
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html"
+    })
+  ],
+
+  mode: "development"
+};
